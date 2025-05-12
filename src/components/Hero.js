3e56b7a2911f7c197093695ce1ef9bdc001a4233
maggi <margarita.herazo.i@gmail.com>
2025-05-12 10:52:32 -0300

    adding img, text and style hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import heroImage from '../assets/imgs/hero1.jpg';
 
+const Hero = () => {
+  return (
+    
+    <div className="hero-section" style={{ backgroundImage: `url(${heroImage})` }}>
+      <div className="container text-center">
+        <h1 className="display-4 text-white">To see your wholeness is to find your light</h1>
+        <a href="/therapies" className="btn btn-primary">Learn More</a>
+      </div>
+    </div>
+  );
+}
 
-function Hero() {
-    return (
-      <section className="hero">
-        <h1>Welcome to Eye of Light</h1>
-        <p>Discover your inner healing</p>
-      </section>
-    );
-  }
-  
-  export default Hero;
-  
+export default Hero;
